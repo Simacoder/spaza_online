18adebb73e1399549bb539f9c414220f195ae98b
Simanga <simacoder@hotmail>
2024-06-03 22:25:51 +0200

    created the products pae

--- spaza_online/spaza_app/views.py
+++ spaza_online/spaza_app/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from . models import Product
 
 # Create your views here.
 
@@ -8,6 +10,8 @@ def home(request):
     return render(request, "spaza_app/home.html")
 
 class CategoryView(View):
-    def get(self, request):
-        return render(request, "spaza_app/category.html")
+    def get(self, request, val):
+        product = Product.objects.filter(category=val)
+        title = Product.objects.filter(category=val).values('title')
+        return render(request, "spaza_app/category.html", locals())
 
